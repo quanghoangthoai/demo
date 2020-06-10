6a4ec048d30565375f395e0710b71884b4da5db0
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2020-06-10 15:56:22 +0700

    .

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Carbon\Carbon;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+   use AuthenticatesUsers;
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    public function authenticated(Request $request, $user)
+{
+
+    $user->timestamps = false;
+    $user->last_login_at = Carbon::now('Asia/Ho_Chi_Minh')->toDateTimeString();
+    $user->last_login_ip = $request->ip();
+    $user->save();
+  if(auth())
+{
+
+    return redirect('/products');
+
+ }
+
+    return redirect('/products');
+
+
+}
+
+    public function logout(Request $request) {
+      Auth::logout();
+      return redirect('/login');
+    }
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
